2d40569fc83034a4db62a069b7f2e7be21d58051
Jeeseob <jeeseob5761@gmail.com>
2022-04-12 16:40:46 +0900

    data 형식

--- src/Data.java
+++ src/Data.java
@@ -1,7 +1,40 @@
-public class Data {
-    Long id;
+import java.time.LocalDateTime;
 
-    public Data(Long id) {
-        this.id = id;
+public class Data {
+    public Data(Long nodeId, LocalDateTime timeStamp, Long sequence, double temperature, double voltage, double electricCurrent) {
+        this.nodeId = nodeId;
+        this.timeStamp = timeStamp;
+        this.sequence = sequence;
+
+        this.temperature = temperature;
+        this.voltage = voltage;
+        this.electricCurrent = electricCurrent;
+    }
+
+    private Long nodeId;
+
+    private LocalDateTime timeStamp;
+    private Long sequence;
+
+    private double temperature;
+    private double voltage;
+    private double electricCurrent;
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("{");
+        stringBuilder.append("node_id : " + nodeId + ", ");
+        stringBuilder.append("timeStamp : " + "\"" + timeStamp.toString() + "\"" + ", ");
+        stringBuilder.append("sequence : " + sequence + ", ");
+
+        stringBuilder.append("temperature : " + temperature + ", ");
+        stringBuilder.append("voltage : " + voltage + ", ");
+        stringBuilder.append("electricCurrent : " + electricCurrent);
+
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
     }
 }
